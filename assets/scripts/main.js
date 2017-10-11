56d0e46dfca37a5c9087b361a07cb036d697340e
rthel <rthel@foxmail.com>
2017-10-11 11:16:05 +0800

    mobile adjustment.

---
+++ assets/scripts/main.js
@@ -0,0 +1,23 @@
+var screenHeight = window.screen.height;
+function sideBarResizer() {
+    'use strict';
+    var viewPortHeight = 0;
+    var DEFAULTSIDEBARPADDING = 0.4;
+    var DEFAULTSIDEBARIMAGE = 127;
+
+    function getRatio() {
+        viewPortHeight = window.innerHeight;
+        return viewPortHeight/screenHeight;
+    }
+
+    function resizeByRatio(original, ratio) {
+        return original * ratio();
+    }
+    var sideBarLogo = document.querySelector('.logo-title');
+    var sideBarImage = document.querySelector('.sidebar-image');
+    sideBarLogo.style.paddingTop = resizeByRatio(DEFAULTSIDEBARPADDING, getRatio)*100+'%';
+    sideBarImage.style.width = resizeByRatio(DEFAULTSIDEBARIMAGE, getRatio)*1.2+'px';
+}
+
+sideBarResizer();
+window.onresize = sideBarResizer;
