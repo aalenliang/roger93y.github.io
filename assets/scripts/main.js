73d68b046ed508be3b3c452110d31e0d18ab7a92
rthel <rthel@foxmail.com>
2018-04-11 21:31:50 +0800

    add reading mode.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,41 @@
 var screenHeight = window.screen.height;
+var FULLSCREEN_BUTTON = document.querySelector(".reading-mode");
+var SIDEBAR = document.querySelector(".sidebar.sidebar-mobile");
+var PAGE_CONTENT = document.querySelector(".page-content");
+var PAGE_HEADER = document.querySelector(".site-header.blur");
+
+function activeReadingMode() {
+    FULLSCREEN_BUTTON.style.backgroundImage = "url(/assets/quitfull.svg)"
+    SIDEBAR.classList.add("reading-mode-empty");
+    PAGE_CONTENT.classList.add("reading-mode-full");
+    PAGE_HEADER.classList.add("reading-mode-full");
+}
+
+function cancelReadingMode() {
+    FULLSCREEN_BUTTON.style.backgroundImage = "url(/assets/full.svg)"
+    SIDEBAR.classList.remove("reading-mode-empty");
+    PAGE_CONTENT.classList.remove("reading-mode-full");
+    PAGE_HEADER.classList.remove("reading-mode-full");
+}
+
+function toggleReadingMode() {
+    var isFullScreen = localStorage.getItem("isFullScreen");
+    if(isFullScreen == null) {
+        localStorage.setItem("isFullScreen", "0");
+    }
+    if(isFullScreen === "0" || isFullScreen == null) {
+        activeReadingMode();
+        localStorage.setItem("isFullScreen", "1");
+    } else {
+        cancelReadingMode();
+        localStorage.setItem("isFullScreen", "0");
+    }
+}
+
+FULLSCREEN_BUTTON.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    toggleReadingMode()
+})
 
 function sideBarResizer() {
     'use strict';
@@ -31,3 +68,12 @@ function throttle(method, context) {
 window.onresize = function () {
     throttle(sideBarResizer);
 }
+
+-function() {
+    var isFullScreen = localStorage.getItem("isFullScreen");
+    if(isFullScreen === "1") {
+        activeReadingMode();
+    } else {
+        cancelReadingMode();
+    }
+}();
